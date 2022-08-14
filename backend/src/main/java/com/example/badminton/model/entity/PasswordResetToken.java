5b04f38549e06460d6a80af2064d22f2f024350b
euni.cheng <euni.cheng@linecorp.com>
2022-08-14 23:43:48 +0800

    fix: Auto => Identity, or SQLGrammarException: error performing isolated work

--- backend/src/main/java/com/example/badminton/model/entity/PasswordResetToken.java
+++ backend/src/main/java/com/example/badminton/model/entity/PasswordResetToken.java
@@ -21,7 +21,7 @@ public class PasswordResetToken {
     private static final int EXPIRATION = 60 * 24;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
