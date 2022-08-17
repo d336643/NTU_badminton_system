bb455b21f0db385ad019d1b8d57336d879a6afb2
Alsion <alison610310@gmail.com>
2022-08-16 23:32:54 -0700

    check identity for admin

--- frontend/src/utilities/checkIdentity.js
+++ frontend/src/utilities/checkIdentity.js
@@ -5,6 +5,7 @@ const checkIdentity = async () => {
     if (!login) return "guest";
     else {
         if (localStorage.getItem("name") === "ntu_badminton") return "manager";
+        else if (localStorage.getItem("name").indexOf("_admin") !== -1) return "manager";
         else return "competitor";
     }
 }
