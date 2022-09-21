6bd4fb4d3951c09370a7ef765cd536a5b6032f72
euni.cheng <euni.cheng@linecorp.com>
2022-09-22 00:26:25 +0800

    update mix double url

--- frontend/src/ScheduleGraph/Tournament.js
+++ frontend/src/ScheduleGraph/Tournament.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Container from '@mui/material/Container';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
@@ -21,16 +21,16 @@ const Root = styled('div')(({ theme }) => ({
 }));
 
 const srcHTTP = ["https://drive.google.com/file/d/1MRSo66xyZNkKCxyizIsEcXcP0oAPs-TZ/preview", "https://drive.google.com/file/d/1_gzjUnC2uTlUQW20W4d3aOf134QYyE5n/preview",
-    "https://drive.google.com/file/d/16pQ_P2tN_29yJ1fwsMs8YrIy3WO6bhld/preview", "https://drive.google.com/file/d/1JSmhcOGKoKM6Md7PTe2GtvHIe_8NbDai/preview", 
-    "https://drive.google.com/file/d/18ixnsvvJTbtzYyklFrRLbkKkfmsxLokB/preview"]
-    
-function Tournament({dataId}) {
-    // const navigate = useNavigate();
-    return (
-        // <Root>
-            <iframe src={srcHTTP[(dataId)]} width="100%" height="800" allow="autoplay" />
-        // </Root>
-    );
+  "https://drive.google.com/file/d/16pQ_P2tN_29yJ1fwsMs8YrIy3WO6bhld/preview", "https://drive.google.com/file/d/1JSmhcOGKoKM6Md7PTe2GtvHIe_8NbDai/preview",
+  "https://drive.google.com/file/d/1DHdT1-MNKLbA-vAM6idXLmbjd8Lo1uGL/preview"]
+
+function Tournament({ dataId }) {
+  // const navigate = useNavigate();
+  return (
+    // <Root>
+    <iframe src={srcHTTP[(dataId)]} width="100%" height="800" allow="autoplay" />
+    // </Root>
+  );
 }
 
 export default Tournament;
