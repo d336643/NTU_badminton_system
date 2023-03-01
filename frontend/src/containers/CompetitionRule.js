ba5ca5b59e198426326291913077ff62578b5fd2
Alsion <75669495+Cyalisonliu@users.noreply.github.com>
2023-03-01 10:08:57 +0800

    Update competition rul

--- frontend/src/containers/CompetitionRule.js
+++ frontend/src/containers/CompetitionRule.js
@@ -13,7 +13,7 @@ function ShowPdf({isLogin}) {
             }} 
         >{/*minHeight: '80vh'*/}
             <h3 style={{ marginBottom: '20px', marginTop: "20px" }}>競賽章程</h3>
-            <iframe src="https://drive.google.com/file/d/1-BDqpVvivcQLwha3tgYKfya9dLI3H7z0/preview" width="100%" height="720" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1Fc-rw4WrSxc7BpEmEDySfatRKnRJmfbj/preview" width="100%" height="720" allow="autoplay"></iframe>
             {isLogin ?
                 // <Button 
                 //     sx={{mt: '3%'}}
