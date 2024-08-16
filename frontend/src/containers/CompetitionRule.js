5f6b12556ad22378e540af8d89d153599ec33694
Alison <75669495+Cyalisonliu@users.noreply.github.com>
2024-08-15 22:36:38 -0400

    update: competition rules link

--- frontend/src/containers/CompetitionRule.js
+++ frontend/src/containers/CompetitionRule.js
@@ -15,31 +15,15 @@ function ShowPdf({isLogin}) {
             }} 
         >{/*minHeight: '80vh'*/}
             <h3 style={{ marginBottom: '20px', marginTop: "20px" }}>競賽章程</h3>
-            <iframe src="https://drive.google.com/file/d/15OGDNu_PMUufYroIrCluPqunlKe0Fmmb/preview" width="100%" height="720" allow="autoplay"></iframe>
-            {isLogin ?
-                // <Button 
-                //     sx={{mt: '3%'}}
-                //     variant="contained"
-                //     onClick={() => navigate('/register')}
-                // >
-                //     前往報名
-                // </Button>
-                <Button 
-                    sx={{mt: '20px'}}
-                    variant="outlined"
-                    onClick={() => navigate('/')}
-                >
-                    返回主頁面
-                </Button>
-                : 
-                <Button 
-                    sx={{mt: '20px'}}
-                    variant="outlined"
-                    onClick={() => navigate('/')}
-                >
-                    返回主頁面
-                </Button>
-            }
+            
+            <iframe src="https://drive.google.com/file/d/13kEzQr40hU17Imii54eocp0soW0Wgcnu/preview" width="100%" height="720" allow="autoplay"></iframe>
+            <Button 
+                sx={{mt: '20px'}}
+                variant="outlined"
+                onClick={() => navigate('/')}
+            >
+                返回主頁面
+            </Button>
         </Container>
     );
 }
