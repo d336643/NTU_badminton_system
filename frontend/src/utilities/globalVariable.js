a06db28629e110814488a5883c8976994269ecc1
euni cheng <d336643@gmail.com>
2025-09-27 13:09:45 +0800

    update: 114-1 schedule link

--- frontend/src/utilities/globalVariable.js
+++ frontend/src/utilities/globalVariable.js
@@ -4,7 +4,7 @@ export const REGISTRATION_OPEN = false
 
 export const CUP_TYPE = 'freshman' //'freshman' or 'ntucup'
 
-export const SCHEDULE_TIME_LINK = 'https://drive.google.com/file/d/1QpFpHcMK0wk3m_9lTvGCuqb_M_IA4A9f/preview'
+export const SCHEDULE_TIME_LINK = 'https://drive.google.com/file/d/1JXby7MZ2rSdr_y6yxgG_1e6_vEO4_aMv/preview'
 
 export const RULE_LINK = 'https://drive.google.com/file/d/1CesJwYCF0y8NQwjgjgzUNEbn_9D20ggy/preview'
 
@@ -17,14 +17,14 @@ export const GROUP_REGISTRATION_FORM_LINK = 'https://forms.gle/VyxRqmpKxKguffEj6
 // 女雙
 // 混雙
 export const SHEET_URL = [
-    "https://drive.google.com/file/d/1AOlxQoMm_iVHbH9rFpvTx1oOYVghIODu/preview",
-    "https://drive.google.com/file/d/1G3Nzh9obu9YQr_bH6s9UJsAxMaktiD9P/preview",
-    "https://drive.google.com/file/d/1sh7jB7_cHRQlN10HBRX8H4j8YlD6Lwd6/preview",
-    "https://drive.google.com/file/d/1OOb6m-TJD6WVhhrAk4QpxcL_MGsD53QY/preview",
-    "https://drive.google.com/file/d/1Pu85zfzNf2104F2Qgodh74xf7-tUi_KT/preview",
+    "https://drive.google.com/file/d/1QD8yJgf1slwl3D6XJ_3tHUaOfNFrWIAx/preview",
+    "https://drive.google.com/file/d/1gArrGSGxEfz2vhq_j9sRFTkAG8rc-40z/preview",
+    "https://drive.google.com/file/d/1zahk3ylPIw71vlpT9DEhRklQlRXqki-W/preview",
+    "https://drive.google.com/file/d/1dF3RSsLXxUSI0S-WdM1NNgTnyROPf97i/preview",
+    "https://drive.google.com/file/d/1pSmadY_k2ziY5IsWN4OTzZv2sQrER6F9/preview",
 ]
 
-export const GAME_RULE = "四角循環皆取二名晉級、三角循環皆取一名晉級"
+export const GAME_RULE = "四角循環皆取二名晉級、三角循環除女單取二名晉級外，其餘項目皆為一組取一名晉級"
 
 export const BANK_CODE = "700"
 export const BANK_ACCOUNT = "00015610593227"
