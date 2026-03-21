0444bf0cfe0417fdd0469ff333f968616a41c679
d336643 <d336643@gmail.com>
2026-03-21 21:39:04 +0800

    change to 114-2 NTU CUP

--- frontend/src/utilities/globalVariable.js
+++ frontend/src/utilities/globalVariable.js
@@ -1,14 +1,14 @@
-export const SEMESTER = '114-1'
+export const SEMESTER = '114-2'
 
-export const REGISTRATION_OPEN = false
+export const REGISTRATION_OPEN = true
 
-export const CUP_TYPE = 'freshman' //'freshman' or 'ntucup'
+export const CUP_TYPE = 'ntucup' //'freshman' or 'ntucup'
 
 export const SCHEDULE_TIME_LINK = 'https://drive.google.com/file/d/1JXby7MZ2rSdr_y6yxgG_1e6_vEO4_aMv/preview'
 
-export const RULE_LINK = 'https://drive.google.com/file/d/1CesJwYCF0y8NQwjgjgzUNEbn_9D20ggy/preview'
+export const RULE_LINK = 'https://drive.google.com/file/d/1Cf6QTVd1rU-rG8vktaQr_ZHzKLhQ7WaC/preview'
 
-export const GROUP_REGISTRATION_FORM_LINK = 'https://forms.gle/VyxRqmpKxKguffEj6'
+export const GROUP_REGISTRATION_FORM_LINK = 'https://docs.google.com/forms/d/e/1FAIpQLSewtfxo6tidQEr6JF2iCcxQU6eCqyvzfWqF6pEGXlB4Db2fuw/viewform?usp=dialog'
 
 // 複賽賽程圖 
 // 男單
@@ -26,7 +26,7 @@ export const SHEET_URL = [
 
 export const GAME_RULE = "四角循環皆取二名晉級、三角循環除女單取二名晉級外，其餘項目皆為一組取一名晉級"
 
-export const BANK_CODE = "700"
-export const BANK_ACCOUNT = "00015610593227"
-export const BANK_ACCOUNT_NAME = "呂皓晨"
+export const BANK_CODE = "822"
+export const BANK_ACCOUNT = "234540513043"
+export const BANK_ACCOUNT_NAME = "謝宜樺"
 
